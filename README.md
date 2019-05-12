8739b7f2a301610abf030646ef94e2ee7b9a92c5
geekdevers <geekdevmkt@gmail.com>
2019-05-12 20:03:06 -0300

    Static Page
    Static Page

--- README.md
+++
@@ -1,3 +0,0 @@
-# lucashiago.github.io
-Geekers Store - Vue2 (1.0.0)
-Changed static files to a Vue2 technology, Powered by api backend, Powering axios on client vue2 and Saved externally in mongodb;
---
+++ js/custom.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+    const HEAD = '/head/';
+    const BODY = '/body/';
+    const FORMS = '/forms/';
+    const ATOMS = '/atoms/';
+
+    //IF HEAD
+    if ($('#head').find) {
+        $('#head').load('./components' + HEAD + 'head.html');
+    } else {
+        console.error('Cabeçalho não encontrado!');
+    }
+
+    //IF SIDEBAR
+    if ($('#side-bar').find) {
+        $('#side-bar').load('./components' + HEAD + 'sidebar.html');
+    } else {
+        console.error('Não carregou!');
+    }
+
+    //IF MAIN
+    if ($('#main').find) {
+        $('#main').load('./components' + BODY + 'main.html');
+    } else {
+        console.error('Não carregou!');
+    }
+
+    //IF FOOTER
+    if ($('#footer').find) {
+        $('#footer').load('./components' + BODY + 'footer.html');
+    } else {
+        console.error('Não carregou!');
+    }
+
+    //IF PRELOADER
+    if ($('#preloader').find) {
+        $('#preloader').load('./components' + ATOMS + 'preloader.html');
+    } else {
+        console.error('Não carregou!');
+    }
+
+});
