bbbe0e13d7e2e5f58b7f4974127c84bdb0761fca
geekdevers <geekdevmkt@gmail.com>
2019-07-26 13:43:37 -0300

    Updates

---
+++ precache-manifest.3dab019720dbe149855855984632bb69.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "b690bd92ae3dd9f7fc91cf11e90820b5",
+    "url": "/black-dashboard-react/static/media/store_logo.b690bd92.png"
+  },
+  {
+    "revision": "cb340f87fa73a12c1186",
+    "url": "/black-dashboard-react/static/css/main.470d2627.chunk.css"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/black-dashboard-react/static/media/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "01bf6c5924e7092b444c",
+    "url": "/black-dashboard-react/static/js/1.01bf6c59.chunk.js"
+  },
+  {
+    "revision": "b46a1e60e5b4cb2d0205",
+    "url": "/black-dashboard-react/static/js/runtime~main.b46a1e60.js"
+  },
+  {
+    "revision": "6791c03551a51d8ca8da95f039565b0b",
+    "url": "/black-dashboard-react/static/media/great_work.6791c035.png"
+  },
+  {
+    "revision": "dd0b73a67849f994572e08ba4c4ccb6a",
+    "url": "/black-dashboard-react/static/media/pedeja.dd0b73a6.png"
+  },
+  {
+    "revision": "734fa2ff4e108f68f3e47bf2f8defd25",
+    "url": "/black-dashboard-react/static/media/react_commerce.734fa2ff.png"
+  },
+  {
+    "revision": "cb340f87fa73a12c1186",
+    "url": "/black-dashboard-react/static/js/main.cb340f87.chunk.js"
+  },
+  {
+    "revision": "eb6be414b75c412c13eeee2d2e8390cb",
+    "url": "/black-dashboard-react/static/media/react-logo.eb6be414.png"
+  },
+  {
+    "revision": "da3e929489f64120f440375b20ee0015",
+    "url": "/black-dashboard-react/static/media/lucas.da3e9294.jpg"
+  },
+  {
+    "revision": "bd6820f1a4015e9645e470ea8e7b1501",
+    "url": "/black-dashboard-react/static/media/anime3.bd6820f1.png"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/black-dashboard-react/static/media/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/black-dashboard-react/static/media/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/black-dashboard-react/static/media/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "01bf6c5924e7092b444c",
+    "url": "/black-dashboard-react/static/css/1.ab76e0ee.chunk.css"
+  },
+  {
+    "revision": "437f55575051a1e76a747bde5d7b76f7",
+    "url": "/black-dashboard-react/index.html"
+  }
+];
